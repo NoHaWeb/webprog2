3c85779958b572e1151c102c32db38304d250e2c
NoHaWeb <onlinenorbert@gmail.com>
2024-06-02 22:17:44 +0200

    reg form beosztás opcióval

--- regisztracio_form.php
+++ regisztracio_form.php
@@ -1,4 +1,13 @@
-<h1>KÖNYVTÁR</h1>
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=web2', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$query = "SELECT * FROM beosztasok";
+$beok = $pdo->query($query)->fetchAll(PDO::FETCH_OBJ);
+
+?>
+<h1>KISKERESKEDELEM</h1>
 <hr>
 <h2>Regisztráció</h2>
 
@@ -15,6 +24,16 @@
         <tr>
             <td><label for="jelszo">Jelszó:</label></td>
             <td><input type="password" id="jelszo" name="jelszo" required></td>
+        </tr>
+        <tr>
+            <td><label for="jelszo">Beosztas:</label></td>
+            <td>
+                <select id="beosztas" name="beosztas">
+                    <?php foreach($beok as $beo): ?>
+                        <option value="<?php echo $beo->beo_id ?>"><?php echo $beo->beo_nev ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </td>
         </tr>
         <tr>
             <td><input type="submit" value="Regisztráció"></td>
